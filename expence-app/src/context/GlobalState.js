7c6be66898eeac8390113950a660376e18073a56
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2020-10-29 20:48:59 +0200

    correct-globalstate-file

--- expence-app/src/context/GlobalState.js
+++ expence-app/src/context/GlobalState.js
@@ -1,24 +1,52 @@
-import React, { createContext, useReducer } from "react";
+import React, { createContext, useReducer, useEffect } from "react";
+import AppReducer from "./AppReducer";
 
 const initialState = {
-  incomeTransactions: [
-    { id: 1, incomeText: "motobike sold", incomeAmount: 3500 },
-    { id: 2, incomeText: "Salary", incomeAmount: 10000 },
-    { id: 3, incomeText: "Bonus", incomeAmount: 8000 },
-  ],
-  expenseTransactions: [
-    { id: 1, expenseText: "hospital expenses", expenseAmount: 2500 },
-    { id: 2, expenseText: "taxes", expenseAmount: 3000 },
-    { id: 3, expenseText: "shopping Therapy", expenseAmount: 750 },
-  ],
+  incomeTransactions:
+    JSON.parse(localStorage.getItem("incomeTransactions")) || [],
+  expenseTransactions:
+    JSON.parse(localStorage.getItem("expenseTransactions")) || []
 };
+
 export const GlobalContext = createContext(initialState);
 
-export const GlobalContextProvider=({children})=>{
-    const[state,dispatch]=useReducer(AppReducer, inistialState)
+export const GlobalContextProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(AppReducer, initialState);
 
-    return(
-<GlobalContext.Provider
+  useEffect(() => {
+    localStorage.setItem(
+      "incomeTransactions",
+      JSON.stringify(state.incomeTransactions)
+    );
+    localStorage.setItem(
+      "expenseTransactions",
+      JSON.stringify(state.expenseTransactions)
+    );
+  });
+
+  const deleteTransaction = id => {
+    dispatch({
+      type: "DELETE_TRANSACTION",
+      payload: id
+    });
+  };
+
+  const addIncome = incomeTransaction => {
+    dispatch({
+      type: "ADD_INCOME",
+      payload: incomeTransaction
+    });
+  };
+
+  const addExpense = expenseTransaction => {
+    dispatch({
+      type: "ADD_EXPENSE",
+      payload: expenseTransaction
+    });
+  };
+
+  return (
+    <GlobalContext.Provider
       value={{
         incomeTransactions: state.incomeTransactions,
         expenseTransactions: state.expenseTransactions,
@@ -29,5 +57,5 @@ export const GlobalContextProvider=({children})=>{
     >
       {children}
     </GlobalContext.Provider>
-    )
-}
+  );
+};
