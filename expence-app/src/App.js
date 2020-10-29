0e86091ecc4f1802d45749da88a1077ff11761eb
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2020-10-29 19:11:08 +0200

    adding-to-app-file

--- expence-app/src/App.js
+++ expence-app/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Header from "./components/Header";
+import Balance from "./components/Balance";
 import "./App.css";
 
 const App = () => {
@@ -7,6 +8,7 @@ const App = () => {
     <div className="container">
       <div className="app-wrapper">
         <Header/>
+        <Balance/>
       </div>
       
     </div>
